82e52ba2227359097b1fb6fc2d5e6a23b435f988
jamesasims <37858459+jamesasims@users.noreply.github.com>
2018-08-22 14:06:38 -0700

    Acme Cover Screen

---
+++ AcmeAccounting/AcmeAccounting/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace AcmeAccounting
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Welcome to Acme Accounting Systems \nRemember, we're \"accounting\" on you!");
+            Console.Read();
+        }
+    }
+}
