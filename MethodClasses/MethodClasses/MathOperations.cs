e446c540212a0590e0009574c2f8b133a38a8517
jamesasims <37858459+jamesasims@users.noreply.github.com>
2018-09-20 16:23:58 -0700

    Methods/Classes

---
+++ MethodClasses/MethodClasses/MathOperations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClasses
+{
+    public class MathOperations
+    {
+
+        public static int AdditionOperation(int input, int answer)
+        {
+            answer = input + 8;
+            return answer;
+           
+        }
+
+            public int answer { get; set; }
+            public int input { get; set; }
+
+
+    }
+
+        
+ }
+
---
+++ MethodClasses/MethodClasses/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClasses
+{
+    public class Program : MathOperations
+    {
+
+        public static MathOperations MathOperations { get; set; }
+       
+
+        public static void Main(string[] args)
+        {
+            MathOperations = new MathOperations();
+            Console.WriteLine("Please provide a number: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            
+
+            Console.WriteLine(MathOperations.AdditionOperation(MathOperations.answer, MathOperations.answer));
+            
+           
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
