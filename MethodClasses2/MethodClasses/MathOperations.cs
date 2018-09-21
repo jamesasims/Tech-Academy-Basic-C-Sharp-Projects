900e2d66ac40322ecc9e77f6dff3c88ee347abbc
jamesasims <37858459+jamesasims@users.noreply.github.com>
2018-09-21 14:56:19 -0700

    Methods/Classes2

---
+++ MethodClasses2/MethodClasses/MathOperations.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClasses
+{
+    public class MathOperations
+    {
+
+        public static int AdditionOperation(int input, int answer)
+        {
+            answer = input + 26;
+            return answer;
+           
+        }
+        public static int MultiplicationOperation(int input, int answer2)
+        {
+            answer2 = input * 5;
+            return answer2;
+        }
+        public static int SubtractionOperation(int input, int answer3)
+        {
+            answer3 = input - 4;
+            return answer3;
+        }
+            
+            public int answer3 { get; set; }
+            public int answer2 { get; set; }
+            public int answer { get; set; }
+            public int input { get; set; }
+
+
+    }
+
+        
+ }
+
