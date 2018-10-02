020eafdfb011e2e430e2212c5351c6e924398ed0
jamesasims <37858459+jamesasims@users.noreply.github.com>
2018-10-02 01:46:22 -0700

    Methods_Classes3

---
+++ Methods_Classes3/Methods_Classes3/MathOperation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Classes3
+{
+    class MathOperation
+    {
+        public int IntOperation(int i)
+        {
+            int answer1 = i + 15;
+            return answer1;
+        }
+        public int IntOperation(decimal i)
+        {
+            decimal answer1 = i + 20;
+            return Convert.ToInt32(answer1);
+        }
+        public int IntOperation(string i)
+        {
+            if (int.TryParse(i, out int inputInt))
+            {
+                int answer1 = inputInt + 40;
+                return answer1;
+            }
+
+            return 0000;
+        }
+    }
+
+}
---
+++ Methods_Classes3/Methods_Classes3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Classes3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperation Math = new MathOperation();
+
+
+            Console.WriteLine(Math.IntOperation(12));
+            Console.WriteLine(Math.IntOperation(10.6m));
+            Console.WriteLine(Math.IntOperation("lol"));
+            Console.ReadLine();
+
+        }
+    }
+
+}
