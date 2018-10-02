a229dc21a8561aa3bf5bb6f92aeb474e988a5a43
jamesasims <37858459+jamesasims@users.noreply.github.com>
2018-10-02 01:44:33 -0700

    Methods/Classes2

---
+++ Methods_classes2/Methods_classes2/MathOperations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_classes2
+{
+    public class MathOperations
+    {
+        public void AdditionOperation(int par1, int par2)
+        {
+            int answer1 = par1 + 5;
+            Console.WriteLine(par2);
+
+        }
+
+    }
+
+}
---
+++ Methods_classes2/Methods_classes2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_classes2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperations Math = new MathOperations();
+            int value1 = 10;
+            int value2 = 20;
+            Math.AdditionOperation(5, 7);
+            Math.AdditionOperation(value1, value2);
+            Console.ReadLine();
+        }
+    }
+
+}
